b63f95c2fa40d9729cae9e480d471ad7391e0040
Saswat <saswatsubham11@gmail.com>
2024-03-10 19:22:59 +0530

    created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
@@ -15,5 +16,27 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     // res.status(500).json(error.message);
     next(error); //this calls the middleware implemented in the index.js
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      next(errorHandler(401, "Invalid Credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...restInfo } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(restInfo);
+  } catch (error) {
+    next(error);
   }
 };
