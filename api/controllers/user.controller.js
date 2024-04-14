e9eebe1f29de91d783699779ffcfa098542553e0
Saswat <saswatsubham11@gmail.com>
2024-04-15 00:51:54 +0530

    added complete delete functionality (created api and linked to frontend)

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -36,3 +36,18 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    //the user.id we are getting from verifyUser.js (req.user = user)
+    return next(errorHandler(401, "You can only delete your own account!"));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted successfully");
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -33,6 +33,18 @@ const userSlice = createSlice({
     updateUserFailure: (state, action) => {
       (state.loading = false), (state.error = action.payload);
     },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -43,6 +55,9 @@ export const {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
- } = userSlice.actions;
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
