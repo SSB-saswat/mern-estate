83151e1a8a83b986137ec6dbf66beeeaf525ef99
Saswat <saswatsubham11@gmail.com>
2024-06-26 20:09:37 +0530

    added configs for deplyoment

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.routing.js";
 
+import path from "path";
+
 dotenv.config();
 
 mongoose
@@ -17,6 +19,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -30,6 +34,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 // middleware to handle possible errors
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -40,5 +49,3 @@ app.use((err, req, res, next) => {
     message,
   });
 });
-
-
